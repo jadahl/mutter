00ff159fb35dac4f384db524d822cd44ed49d4c9
Jonas Ådahl <jadahl@gmail.com>
2015-04-09 11:47:23 +0800

    nested: Use XDefineCursor to hide server cursor
    Set a generated empty cursor to hide the cursor when running in nested
    compositor mode. We should not use XFixes for this, since XFixes hides
    the parent display server cursor globally, which means that if the
    pointer leaves the compositor stage when the cursor was hidden it'd stay
    hidden.
    
    https://bugzilla.gnome.org/show_bug.cgi?id=747089

--- src/backends/x11/meta-cursor-renderer-x11.c
+++ src/backends/x11/meta-cursor-renderer-x11.c
@@ -28,6 +28,8 @@
 
 #include <X11/extensions/Xfixes.h>
 
+#include <meta/util.h>
+
 #include "meta-backend-x11.h"
 #include "meta-stage.h"
 
@@ -38,6 +40,69 @@ struct _MetaCursorRendererX11Private
 typedef struct _MetaCursorRendererX11Private MetaCursorRendererX11Private;
 
 G_DEFINE_TYPE_WITH_PRIVATE (MetaCursorRendererX11, meta_cursor_renderer_x11, META_TYPE_CURSOR_RENDERER);
+
+static void
+show_server_cursor (MetaBackendX11 *backend, MetaCursor cursor)
+{
+  Display *xdisplay = meta_backend_x11_get_xdisplay (backend);
+  Window xwindow = meta_backend_x11_get_xwindow (backend);
+
+#ifdef HAVE_WAYLAND
+  if (meta_is_wayland_compositor ())
+    {
+      Cursor xcursor = meta_cursor_create_x_cursor (xdisplay, cursor);
+      XDefineCursor (xdisplay, xwindow, xcursor);
+      XFlush (xdisplay);
+      XFreeCursor (xdisplay, xcursor);
+    }
+  else
+#endif
+    XFixesShowCursor (xdisplay, xwindow);
+}
+
+#ifdef HAVE_WAYLAND
+static Cursor
+create_empty_cursor (Display *xdisplay)
+{
+  XcursorImage *image;
+  XcursorPixel *pixels;
+  Cursor xcursor;
+
+  image = XcursorImageCreate (1, 1);
+  if (image == NULL)
+    return None;
+
+  image->xhot = 0;
+  image->yhot = 0;
+
+  pixels = image->pixels;
+  pixels[0] = 0;
+
+  xcursor = XcursorImageLoadCursor (xdisplay, image);
+  XcursorImageDestroy (image);
+
+  return xcursor;
+}
+#endif
+
+static void
+hide_server_cursor (MetaBackendX11 *backend)
+{
+  Display *xdisplay = meta_backend_x11_get_xdisplay (backend);
+  Window xwindow = meta_backend_x11_get_xwindow (backend);
+
+#ifdef HAVE_WAYLAND
+  if (meta_is_wayland_compositor ())
+    {
+      Cursor empty_xcursor = create_empty_cursor (xdisplay);
+      XDefineCursor (xdisplay, xwindow, empty_xcursor);
+      XFlush (xdisplay);
+      XFreeCursor (xdisplay, empty_xcursor);
+    }
+  else
+#endif
+    XFixesHideCursor (xdisplay, xwindow);
+}
 
 static gboolean
 meta_cursor_renderer_x11_update_cursor (MetaCursorRenderer *renderer,
@@ -52,8 +117,6 @@ meta_cursor_renderer_x11_update_cursor (MetaCursorRenderer *renderer,
   if (xwindow == None)
     return FALSE;
 
-  Display *xdisplay = meta_backend_x11_get_xdisplay (backend);
-
   gboolean has_server_cursor = FALSE;
 
   if (cursor_sprite)
@@ -63,24 +126,14 @@ meta_cursor_renderer_x11_update_cursor (MetaCursorRenderer *renderer,
       if (cursor != META_CURSOR_NONE &&
           meta_cursor_sprite_get_current_scale (cursor_sprite) == 1)
         {
-          Cursor xcursor = meta_cursor_create_x_cursor (xdisplay, cursor);
-          XDefineCursor (xdisplay, xwindow, xcursor);
-          XFlush (xdisplay);
-          XFreeCursor (xdisplay, xcursor);
-
+          show_server_cursor (backend, cursor);
           has_server_cursor = TRUE;
         }
     }
 
-  if (has_server_cursor != priv->server_cursor_visible)
-    {
-      if (has_server_cursor)
-        XFixesShowCursor (xdisplay, xwindow);
-      else
-        XFixesHideCursor (xdisplay, xwindow);
-
-      priv->server_cursor_visible = has_server_cursor;
-    }
+  if (!has_server_cursor && has_server_cursor != priv->server_cursor_visible)
+    hide_server_cursor (backend);
+  priv->server_cursor_visible = has_server_cursor;
 
   return priv->server_cursor_visible;
 }
