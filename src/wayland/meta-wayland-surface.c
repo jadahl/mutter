5b510da2056b3705fa62f4545d529383e10d7351
Jonas Ådahl <jadahl@gmail.com>
2015-08-19 11:06:46 +0800

    MetaWaylandSurface: Don't respond to frame callback when role unassigned
    We won't draw a unassigned surface, so don't fool the client that it was
    drawed.
    
    https://bugzilla.gnome.org/show_bug.cgi?id=744932

--- src/wayland/meta-wayland-surface.c
+++ src/wayland/meta-wayland-surface.c
@@ -78,10 +78,37 @@ meta_wayland_surface_set_role (MetaWaylandSurface    *surface,
                                struct wl_resource    *error_resource,
                                uint32_t               error_code)
 {
+  MetaSurfaceActorWayland *surface_actor;
+
   if (surface->role == META_WAYLAND_SURFACE_ROLE_NONE ||
       surface->role == role)
     {
       surface->role = role;
+
+      switch (surface->role)
+        {
+        case META_WAYLAND_SURFACE_ROLE_NONE:
+          g_assert_not_reached();
+          break;
+        case META_WAYLAND_SURFACE_ROLE_XWAYLAND:
+          /* See apply_pending_state for explanation why Xwayland is special here. */
+        case META_WAYLAND_SURFACE_ROLE_CURSOR:
+        case META_WAYLAND_SURFACE_ROLE_DND:
+          wl_list_insert_list (&surface->compositor->frame_callbacks,
+                               &surface->frame_callback_list);
+          wl_list_init (&surface->frame_callback_list);
+          break;
+        case META_WAYLAND_SURFACE_ROLE_WL_SHELL_SURFACE:
+        case META_WAYLAND_SURFACE_ROLE_SUBSURFACE:
+        case META_WAYLAND_SURFACE_ROLE_XDG_SURFACE:
+        case META_WAYLAND_SURFACE_ROLE_XDG_POPUP:
+          surface_actor = META_SURFACE_ACTOR_WAYLAND (surface->surface_actor);
+          meta_surface_actor_wayland_add_frame_callbacks (surface_actor,
+                                                          &surface->frame_callback_list);
+          wl_list_init (&surface->frame_callback_list);
+          break;
+        }
+
       return 0;
     }
   else
@@ -500,6 +527,7 @@ apply_pending_state (MetaWaylandSurface      *surface,
                      MetaWaylandPendingState *pending)
 {
   MetaWaylandCompositor *compositor = surface->compositor;
+  MetaSurfaceActorWayland *surface_actor;
 
   if (pending->newly_attached)
     {
@@ -548,6 +576,9 @@ apply_pending_state (MetaWaylandSurface      *surface,
   switch (surface->role)
     {
     case META_WAYLAND_SURFACE_ROLE_NONE:
+      wl_list_insert_list (&surface->frame_callback_list,
+                           &pending->frame_callback_list);
+      break;
     case META_WAYLAND_SURFACE_ROLE_XWAYLAND:
       /* We cannot wait for the actor to be painted until we reply to the frame
        * callback because this would cause Xwayland to stop posting more damage.
@@ -575,7 +606,8 @@ apply_pending_state (MetaWaylandSurface      *surface,
     case META_WAYLAND_SURFACE_ROLE_XDG_POPUP:
     case META_WAYLAND_SURFACE_ROLE_WL_SHELL_SURFACE:
     case META_WAYLAND_SURFACE_ROLE_SUBSURFACE:
-      meta_surface_actor_wayland_add_frame_callbacks (META_SURFACE_ACTOR_WAYLAND (surface->surface_actor),
+      surface_actor = META_SURFACE_ACTOR_WAYLAND (surface->surface_actor);
+      meta_surface_actor_wayland_add_frame_callbacks (surface_actor,
                                                       &pending->frame_callback_list);
     break;
     }
@@ -954,6 +986,7 @@ wl_surface_destructor (struct wl_resource *resource)
 {
   MetaWaylandSurface *surface = wl_resource_get_user_data (resource);
   MetaWaylandCompositor *compositor = surface->compositor;
+  MetaWaylandFrameCallback *cb, *next;
 
   /* If we still have a window at the time of destruction, that means that
    * the client is disconnecting, as the resources are destroyed in a random
@@ -977,6 +1010,9 @@ wl_surface_destructor (struct wl_resource *resource)
   meta_wayland_compositor_destroy_frame_callbacks (compositor, surface);
 
   g_hash_table_unref (surface->outputs);
+
+  wl_list_for_each_safe (cb, next, &surface->frame_callback_list, link)
+    wl_resource_destroy (cb->resource);
 
   if (surface->resource)
     wl_resource_set_user_data (surface->resource, NULL);
@@ -1013,6 +1049,8 @@ meta_wayland_surface_create (MetaWaylandCompositor *compositor,
 
   surface->buffer_destroy_listener.notify = surface_handle_buffer_destroy;
   surface->surface_actor = g_object_ref_sink (meta_surface_actor_wayland_new (surface));
+
+  wl_list_init (&surface->frame_callback_list);
 
   sync_drag_dest_funcs (surface);
 
