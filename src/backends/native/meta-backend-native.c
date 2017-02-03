e0ff7bf9758992ff213aa4e0f32e11e7e4c2cfdd
Jonas Ådahl <jadahl@gmail.com>
2017-02-03 13:12:01 +0800

    backends/native: Scale relative input motions with monitor scale
    To allow for more natural pointer movements from relative pointer
    devices (e.g. mouse, touchpad, tablet tool in relative mode, etc), scale
    the relative motion from libinput with the scale of the monitor. In
    effect, this means that the pointer movement is twice as fast (physical
    movement vs numbers of pixels passed) as before, but it also means that
    the same physical movement crosses the distance in a GUI no matter if
    it is on a HiDPI monitor or not.

--- src/backends/native/meta-backend-native.c
+++ src/backends/native/meta-backend-native.c
@@ -255,6 +255,29 @@ pointer_constrain_callback (ClutterInputDevice *device,
   constrain_all_screen_monitors (device, monitor_manager, new_x, new_y);
 }
 
+static void
+relative_motion_filter (ClutterInputDevice *device,
+                        guint32             time,
+                        float               x,
+                        float               y,
+                        float              *dx,
+                        float              *dy,
+                        gpointer            user_data)
+{
+  MetaBackend *backend = meta_get_backend ();
+  MetaMonitorManager *monitor_manager =
+    meta_backend_get_monitor_manager (backend);
+  MetaLogicalMonitor *logical_monitor;
+
+  logical_monitor =
+    meta_monitor_manager_get_logical_monitor_at (monitor_manager, x, y);
+  if (!logical_monitor)
+    return;
+
+  *dx *= logical_monitor->scale;
+  *dy *= logical_monitor->scale;
+}
+
 static ClutterBackend *
 meta_backend_native_create_clutter_backend (MetaBackend *backend)
 {
@@ -270,6 +293,7 @@ meta_backend_native_post_init (MetaBackend *backend)
 
   clutter_evdev_set_pointer_constrain_callback (manager, pointer_constrain_callback,
                                                 NULL, NULL);
+  clutter_evdev_set_relative_motion_filter (manager, relative_motion_filter, NULL);
 }
 
 static MetaIdleMonitor *
