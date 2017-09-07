3515462f153756bac6aa0989e4aa32217ab83d52
Marco Trevisan (Treviño) <mail@3v1n0.net>
2017-09-07 18:36:44 +0200

    ClutterText: always use DrawFB for painting
    If not doing this, we might not scale properly when acting on offscreen
    textures

--- clutter/clutter/clutter-text.c
+++ clutter/clutter/clutter-text.c
@@ -2357,13 +2357,7 @@ clutter_text_paint (ClutterActor *self)
   float alloc_height;
   float resource_scale;
 
-  /* FIXME: this should not be needed, but apparently the text-cache
-   * test unit manages to get in a situation where the active frame
-   * buffer is NULL
-   */
-  fb = _clutter_actor_get_active_framebuffer (self);
-  if (fb == NULL)
-    fb = cogl_get_draw_framebuffer ();
+  fb = cogl_get_draw_framebuffer ();
 
   /* Note that if anything in this paint method changes it needs to be
      reflected in the get_paint_volume implementation which is tightly
